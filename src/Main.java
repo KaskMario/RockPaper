9ea7734ee2d03bbdae59b77980a725978b0151f7
Mario Kask <mariokask@gmail.com>
2024-05-26 18:05:29 +0200

    new version

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,43 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scan;
     static String answer;
     static int rand = new Random().nextInt(3);
+    static int gameCount;
 
     public static void main(String[] args) {
+         rockPaperScissors();
 
-        System.out.print("Do you want to play? (y/n) : ");
-        Scanner scan = new Scanner(System.in);
-        if (scan.next().equalsIgnoreCase("y")) {
-            System.out.println("Lets play");
+    }
+    static void rockPaperScissors(){
+        scan = new Scanner(System.in);
+        while (gameCount == 0) {
+            System.out.print("Do you want to play? (y/n) : ");
+            String userInput = scan.next();
 
+            if (userInput.equalsIgnoreCase("y")) {
+                System.out.println("Lets play");
+                gameCount = 1;
+                do {
+                    System.out.print("Choose rock, paper or scissors: ");
+                    answer = scan.next();
+                } while (!answer.equalsIgnoreCase("rock") && !answer.equalsIgnoreCase("paper") && !answer.equalsIgnoreCase("scissors"));
+                System.out.print("CPU chose: ");
+                String cpuChoice = cpuTurn();
+                check(cpuChoice);
+            } else if (userInput.equalsIgnoreCase("n")) {
+                scan.close();
+                break;
+            } else {
+               gameCount = 0;
+            }
 
-            do {
-                System.out.print("Choose rock, paper or scissors: ");
-                answer = scan.next();
-            } while (!answer.equalsIgnoreCase("rock") && !answer.equalsIgnoreCase("paper") && !answer.equalsIgnoreCase("scissors"));
-            System.out.print("CPU chose: ");
-            String cpuChoice = cpuTurn();
-            check(cpuChoice);
-        }else {
-            scan.close();
         }
-
     }
 
 
-    static String  cpuTurn() {
+    static String cpuTurn() {
          String cpuChoice= "";
         switch (rand) {
             case 0 -> cpuChoice = "paper";
@@ -42,34 +53,23 @@ public class Main {
         String aWin = "You Won!!";
         String aLoss = "You lost, CPU WON!!";
         String aTie = "Its a tie!!";
-        //String cpusChoice = String.valueOf(rand);
         if (answer.toLowerCase().equals(cpuChoice)) {
             System.out.println(aTie);
-            //  } if (answer.equalsIgnoreCase("rock") && cpusChoice.equals("1")){
-            //      System.out.println(aTie);
-            //  } if (answer.equalsIgnoreCase("scissors") && cpusChoice.equals("2")){
-            //      System.out.println(aTie);
         }
-        if (answer.equalsIgnoreCase("paper") && cpuChoice.equals("1")) {
-            System.out.println(aWin);
-            //  }// if (answer.equalsIgnoreCase("paper") && cpusChoice.equals("2")) {
-            //  System.out.println(aLoss);
-            //  }// if (answer.equalsIgnoreCase("rock") && cpusChoice.equals("0")) {
-            //  System.out.println(aLoss);
-        }
-        if (answer.equalsIgnoreCase("rock") && cpuChoice.equals("2")) {
+        else if (answer.equalsIgnoreCase("paper") && cpuChoice.equals("rock")) {
             System.out.println(aWin);
         }
-        if (answer.equalsIgnoreCase("scissors") && cpuChoice.equals("0")) {
+        else if (answer.equalsIgnoreCase("rock") && cpuChoice.equals("scissors")) {
             System.out.println(aWin);
-            //  }//if (answer.equalsIgnoreCase("scissors") && cpusChoice.equals("1")) {
-            //  System.out.println(aLoss);
+        }
+        else if (answer.equalsIgnoreCase("scissors") && cpuChoice.equals("paper")) {
+            System.out.println(aWin);
         } else {
             System.out.println(aLoss);
 
         }
 
-
+       gameCount = 0;
     }
 }
 
